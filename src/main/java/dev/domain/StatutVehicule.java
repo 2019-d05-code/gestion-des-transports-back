b0ecffd85feec4d25ce8e606aa1bd8cb6256342e
rmy17 mail = remyvi17@gmail.com <remyvi17@gmail.com>
2019-05-24 12:04:03 +0200

    #13 Correction de l'enumaration

--- src/main/java/dev/domain/StatutVehicule.java
+++ src/main/java/dev/domain/StatutVehicule.java
@@ -1,5 +1,17 @@
 package dev.domain;
 
 public enum StatutVehicule {
-	EN_SERVICE, EN_REPARATION, HORS_SERVICE
+
+	N_SERVICE("en_service"), EN_REPARATION("en_reparation"), HORS_SERVICE("hors_service");
+
+	private String nom;
+
+	private StatutVehicule(String nom) {
+		this.nom = nom;
+	}
+
+	public String toString() {
+		return nom;
+	}
+
 }
