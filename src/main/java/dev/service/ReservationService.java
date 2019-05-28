37f207221e5d8999b14b6d5395838fcc0c03de41
Elen <elen.hayot@gmail.com>
2019-05-28 16:05:21 +0200

    correction code

--- src/main/java/dev/service/ReservationService.java
+++ src/main/java/dev/service/ReservationService.java
@@ -2,9 +2,6 @@ package dev.service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-
-import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,17 +19,17 @@ public class ReservationService {
 
 	@Autowired
 	ReservationRepository reservationRepo;
-	
+
 	@Autowired
-	VehiculeRepo vrepo; 
-	
+	VehiculeRepo vrepo;
+
 	public ReservationService() {
-		
+
 	}
 
 	public ReservationService(ReservationRepository reRepo) {
-		
-	reservationRepo=reRepo;
+
+		reservationRepo = reRepo;
 
 	}
 
@@ -42,58 +39,54 @@ public class ReservationService {
 
 	public void setReservationRepo(ReservationRepository reservationRepo) {
 		this.reservationRepo = reservationRepo;
-	} 
-	
-	//ajouter une réservation 
-	
-	public Reservation ajouterReservation(Reservation reservation) throws ReservationInvalideException {
-		
-		
-		if(reservation.getUneVoiture().getStatutVehicule() != StatutVehicule.EN_SERVICE) {
-			throw new ReservationInvalideException("Le véhicule séletionné n'est actuellement pas en service");
-		}
-		if(reservation.getDateDeReservation()==null || reservation.getDateDeRetour() ==null) {	
-			throw new ReservationInvalideException("on ne peut pas ajouter cette réservation"); 
-		}else {
-		
-		reservationRepo.save(reservation); 
-		return reservation; }
-		
 	}
-	
-	//afficher toutes les réservations 
-	public List<ReservationDTO> afficherToutesLesReservations(){
-		
-		List<ReservationDTO> listeDeReservation = new ArrayList<>(); 
-		List<Reservation> maListesDeReservation = reservationRepo.findAll(); 
-		
+
+	// ajouter une réservation
+
+	public Reservation ajouterReservation(Reservation reservation) throws ReservationInvalideException {
+
+		if (reservation.getUneVoiture().getStatutVehicule() != StatutVehicule.EN_SERVICE) {
+			throw new ReservationInvalideException("Le véhicule séletionné n'est actuellement pas en service");
+		} else if (reservation.getDateDeReservation() == null || reservation.getDateDeRetour() == null) {
+			throw new ReservationInvalideException("on ne peut pas ajouter cette réservation");
+		} else {
+
+			reservationRepo.save(reservation);
+			return reservation;
+		}
+
+	}
+
+	// afficher toutes les réservations
+	public List<ReservationDTO> afficherToutesLesReservations() {
+
+		List<ReservationDTO> listeDeReservation = new ArrayList<>();
+		List<Reservation> maListesDeReservation = reservationRepo.findAll();
+
 		for (Reservation reservation : maListesDeReservation) {
 			ReservationDTO reservationDTO = new ReservationDTO(reservation);
 			listeDeReservation.add(reservationDTO);
 		}
-		return listeDeReservation; 
+		return listeDeReservation;
 	}
 
-	public List<ReservationDTO> afficherListeReservationVehicule(String immatriculation) throws EmptyRepositoryException {
-	
+	public List<ReservationDTO> afficherListeReservationVehicule(String immatriculation)
+			throws EmptyRepositoryException {
+
 		List<ReservationDTO> listeResasDTO = new ArrayList<>();
 		List<Reservation> listeResas = reservationRepo.findByVehiculeImmatriculation(immatriculation);
-	
-		if(listeResas.isEmpty()) {
+
+		if (listeResas.isEmpty()) {
 			throw new EmptyRepositoryException("Aucune réservation enregistrée pour ce véhicule");
 		}
-		
-		for(Reservation resa:listeResas) {
+
+		for (Reservation resa : listeResas) {
 			ReservationDTO resaDTO = new ReservationDTO(resa);
 			listeResasDTO.add(resaDTO);
 		}
-		
+
 		return listeResasDTO;
-		
+
 	}
-	
-	
-	
-	
-	
+
 }
