39c5e1bf546b38265d4cfb5b1f3e280370026eb3
Pennecot <e.pennecot@laposte.net>
2019-05-31 12:55:35 +0200

    ajout d'une méthode de listage des annonces de covoit courrantes

--- src/main/java/dev/service/AnnonceService.java
+++ src/main/java/dev/service/AnnonceService.java
@@ -1,6 +1,7 @@
 package dev.service;
 
 import java.util.stream.Collectors;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -46,19 +47,27 @@ public class AnnonceService {
 		// devront être valorisés (distance, durée)
 		return dto;
 	}
-	
-
-	
-	
 
 	public List<AnnonceDTO> listerSesAnnonces(String emailAnnonceur) throws EmptyRepositoryException {
 		List<Annonce> annonces = annonceRepo.findByAnnonceurEmail(emailAnnonceur)
 				.orElseThrow(() -> new EmptyRepositoryException("Aucune annonce n'a été retrouvée"));
 		return annonces.stream()
-				.map(annonce -> new AnnonceDTO(annonce.getAnnonceur().getEmail(), annonce.getAdressDepart(),
-						annonce.getAdressArrivee(), annonce.getDuree(), annonce.getDistance(),
-						annonce.getDateTimeDepart(), annonce.getImmatriculationVehicule(), annonce.getMarque(),
-						annonce.getModele(), annonce.getPlace()))
+				.map(annonce -> new AnnonceDTO(annonce.getAnnonceur().getNom(), annonce.getAnnonceur().getPrenom(),
+						annonce.getAnnonceur().getEmail(), annonce.getAdressDepart(), annonce.getAdressArrivee(),
+						annonce.getDuree(), annonce.getDistance(), annonce.getDateTimeDepart(),
+						annonce.getImmatriculationVehicule(), annonce.getMarque(), annonce.getModele(),
+						annonce.getPlace()))
+				.collect(Collectors.toList());
+	}
+
+	public List<AnnonceDTO> listerToutesAnnoncesEnCours() throws EmptyRepositoryException {
+		List<Annonce> annoncesEnCours = annonceRepo.findAllCurrent(LocalDateTime.now())
+				.orElseThrow(() -> new EmptyRepositoryException("Aucune annonce en cours n'a été retrouvée"));
+		return annoncesEnCours.stream()
+				.map(a -> new AnnonceDTO(a.getAnnonceur().getNom(), a.getAnnonceur().getPrenom(),
+						a.getAnnonceur().getEmail(), a.getAdressDepart(), a.getAdressArrivee(), a.getDuree(),
+						a.getDistance(), a.getDateTimeDepart(), a.getImmatriculationVehicule(), a.getMarque(),
+						a.getModele(), a.getPlace()))
 				.collect(Collectors.toList());
 	}
 
